2c7d57a43078213046c6ba739af50e44803517b5
dharmveer47 <singhdharmu89@gmail.com>
2023-07-05 23:51:19 +0530

    server deplyed

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = express();
 const port = 8080;
-const mongoURI = process.env.DATABASE_URL;
+// const mongoURI = process.env.DATABASE_URL;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://singhdharmu89:03F1JtdHnSnKLul8@cluster0.wcw3ygc.mongodb.net/subscribers', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", (error) => {
   console.error("MongoDB connection error:", error);
